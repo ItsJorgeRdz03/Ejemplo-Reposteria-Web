a79169a5355f9323979c4e0e9542985daf5e25f8
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-11-17 01:17:42 -0800

    Conectado los productos con el back-end

--- static/scripts/productsScript.js
+++ static/scripts/productsScript.js
@@ -1,3 +1,18 @@
+async function checkLogin() {
+  const res = await fetch("/api/isLogged", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  let respuestaJson = await res.json();
+  respuestaJson = respuestaJson[0];
+  console.log(respuestaJson);
+  if (respuestaJson.res == 1) {
+    document.querySelector(".user-name").innerText = respuestaJson.name;
+  }
+}
+
 async function getData() {
   const res = await fetch("/api/allProducts", {
     method: "GET",
@@ -6,9 +21,45 @@ async function getData() {
     },
   });
   let respuestaJson = await res.json();
-  console.log(respuestaJson);
-  if (respuestaJson[0] == 1) {
+  respuestaJson = respuestaJson[0];
+  if (respuestaJson.res == 1) {
+    const card = document.querySelector(".box");
+    const cont = document.querySelector("main");
+    cont.innerHTML = "";
+    for (let i = 0; i < respuestaJson.data.length; i++) {
+      card.querySelector("img").src = respuestaJson.data[i].img;
+      card.querySelector("h2").innerText = respuestaJson.data[i].nombre;
+      card.querySelector(".product-description").innerText =
+        respuestaJson.data[i].descr;
+      card.querySelector(".product-price").innerText =
+        "$" + respuestaJson.data[i].precio + " MXN";
+      cont.innerHTML += card.innerHTML;
+    }
+    document.querySelectorAll(".product-card").forEach((card) => {
+      card.querySelector(".reserve-btn").addEventListener("click", () => {
+        console.log(card.querySelector("h2").innerText);
+      });
+    });
+  } else {
+    document.querySelector("main").innerHTML = "";
+    document.querySelector("main").innerText = "No hay productos disponibles";
+    document.querySelector("main").style.fontWeight = "bold";
   }
 }
 
+checkLogin();
 getData();
+
+document.querySelector(".logout").addEventListener("click", async () => {
+  const res = await fetch("/api/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  let respuestaJson = await res.json();
+  if (respuestaJson[0].res == 1) {
+    checkLogin();
+    location.href = "/";
+  }
+});
